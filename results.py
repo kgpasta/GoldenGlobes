0200401f64cb526fea0e1093c8c6651b9c633101
Sean Bowen-Williams <spbowenwilliams@gmail.com>
2015-02-18 13:04:07 -0600

    Fixed JSON formatting, still having trouble with unicode on autograder

--- results.py
+++ results.py
@@ -6,24 +6,39 @@ import testing
 import helpers
 
 def process(year, hosts, winners, nominee_table, nominee_list, presenter_list):
+    [x.encode('utf-8') for x in winners]
+    [x.encode('utf-8') for x in nominee_list]
+    [x.encode('utf-8') for x in presenter_list]
     results = {
         "metadata": {
             "year": year,
-            "hosts": {
-                "method": "detected",
-                "method_description": "Regular expressions searching for hosts name's within tweets."
+            "names": {
+                "hosts": {
+                    "method": "detected",
+                    "method_description": "Regular expressions searching for hosts name's (proper nouns) within tweets."
+                    },
+                "nominees": {
+                    "method": "hardcoded",
+                    "method_description": ""
+                    },
+                "awards": {
+                    "method": "hardcoded",
+                    "method_description": ""
+                    },
+                "presenters": {
+                    "method": "hardcoded",
+                    "method_description": ""
+                    }
                 },
-            "nominees": {
-                "method": "hardcoded",
-                "method_description": ""
-                },
-            "awards": {
-                "method": "hardcoded",
-                "method_description": ""
-                },
-            "presenters": {
-                "method": "hardcoded",
-                "method_description": ""
+            "mappings": {
+                "nominees": {
+                    "method": "hardcoded",
+                    "method_description": ""
+                    },
+                "presenters": {
+                    "method": "hardcoded",
+                    "method_description": ""
+                    }
                 }
             },
         "data": {
@@ -31,8 +46,8 @@ def process(year, hosts, winners, nominee_table, nominee_list, presenter_list):
                 "hosts": hosts,
                 "winners": [],
                 "awards": [],
-                "presenters":[],
-                "nominees": nominee_list
+                "presenters": [],
+                "nominees": []
             },
             "structured": {
             }
@@ -56,11 +71,11 @@ def process(year, hosts, winners, nominee_table, nominee_list, presenter_list):
         for value in presenter_list[key]:
             presenterList.append(value)
 
-
     results["data"]["unstructured"]["winners"] = winnerList
     results["data"]["unstructured"]["awards"] = awardsList
     results["data"]["unstructured"]["presenters"] = presenterList
+    results["data"]["unstructured"]["nominees"] = nominee_list
 
     out_file = open('results' + year + '.json', 'w')
-    json.dump(results, out_file)
+    json.dump(results, out_file, encoding="utf-8", ensure_ascii=False)
     out_file.close()
